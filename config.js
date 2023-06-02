48dea26625e116b0c4267f4762ebe6d7057779ed
Step2456 <133936068+Step2456@users.noreply.github.com>
2023-06-02 16:00:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 owner = [
-  ['62895330379186'],
-  ['62895330379186'],
-  ['62895330379186', 'Owner', true]
+  ['6281225765479'],
+  ['6281225765479'],
+  ['6281225765479', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 mods = [] // Want some help?
